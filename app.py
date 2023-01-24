82ae1de7c13e70cd802231562830c5d586349266
salma reda <100859819+salmareda104@users.noreply.github.com>
2023-01-24 18:39:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Fraud Detection App",layout="centered",initial_sidebar_state="expanded")
 
 st.title('Fraud Detection App')
-st.image("fraud_img.jpeg")
+st.image("fraud_img.jpg")
 # Take inputs from user
 
 amt = st.number_input("Transaction Amount")
